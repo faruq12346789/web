3af064091e667403984b9754cc05786ee435b374
faruq123456789 <faruqkusuma38@gmail.com>
2024-09-08 19:38:57 +0700

    update

--- script.js
+++ script.js
@@ -34,10 +34,8 @@ async function friendListsElement() {
 
 friendListsElement();
 
-// Get the button
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
